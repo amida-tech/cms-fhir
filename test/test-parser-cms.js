ee7b7f1d4cb69356afe44beee93833cd0e9b0264
Alex S <alexey.shchelokov@gmail.com>
2015-08-11 18:20:50 -0400

    fix: returns error object on failure

--- test/test-parser-cms.js
+++ test/test-parser-cms.js
@@ -9,26 +9,15 @@ var split = require('split');
 var bbcms = require("../index");
 var bbm = require('blue-button-model');
 
-describe('parser.js', function () {
-    it('CMS parser test', function (done) {
+describe('CMS parser/converter test', function () {
+    it('sample.txt input file', function (done) {
         var istream = fs.createReadStream(__dirname + '/fixtures/sample.txt', 'ascii');
 
         expect(istream).to.exist;
 
-        //convert string into JSON
-        //var result = bbcms.parseText(txtfile);
-        /*var result = bbcms.test(istream.pipe(split()), function(result) {
-            expect(result).to.exist;
-            console.log(result[0]);
-            done();
-        }, function(e) { 
-            console.log(e);
-            done(e);
-        } );*/
-
         istream.pipe(split()).pipe(new bbcms.CmsFile2Object())
             .on('data', function (data) {
-                console.log(JSON.stringify(data, null, '    '));
+                //console.log(JSON.stringify(data, null, '    '));
             })
             .on('finish', function () {
                 done();
@@ -37,38 +26,44 @@ describe('parser.js', function () {
                 done(error);
             });
 
-        /*var valid = bbm.validator.validateDocumentModel(result);
-
-        if (!valid) {
-            console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-        }
-
-        expect(valid).to.be.true;
-        */
-
     });
+
 
     it('CMS parser/converter test', function (done) {
         var istream = fs.createReadStream(__dirname + '/fixtures/sample.txt', 'ascii');
 
         expect(istream).to.exist;
 
-        //convert string into JSON
-        //var result = bbcms.parseText(txtfile);
-        /*var result = bbcms.test(istream.pipe(split()), function(result) {
-            expect(result).to.exist;
-            console.log(result[0]);
-            done();
-        }, function(e) { 
-            console.log(e);
-            done(e);
-        } );*/
+        istream.pipe(split())
+            .pipe(new bbcms.CmsFile2Object())
+            .pipe(new bbcms.IntObjToFhirStream())
+            .on('data', function (data) {
+                if( (data instanceof Error)) {
+                    done( new Error('Error expected'));
+                }
+                //console.log(JSON.stringify(data, null, '    '));
+            })
+            .on('finish', function () {
+                done();
+            })
+            .on('error', function (error) {
+                //done(error);
+            });
+
+    });
+    
+    it('buggy input', function (done) {
+        var istream = fs.createReadStream(__dirname + '/test-parser-cms.js', 'ascii');
+
+        expect(istream).to.exist;
 
         istream.pipe(split())
             .pipe(new bbcms.CmsFile2Object())
             .pipe(new bbcms.IntObjToFhirStream())
             .on('data', function (data) {
-                console.log(JSON.stringify(data, null, '    '));
+                    if( !(data instanceof Error)) {
+                    done( new Error('Error expected'));
+                }
             })
             .on('finish', function () {
                 done();
@@ -77,35 +72,6 @@ describe('parser.js', function () {
                 done(error);
             });
 
-        /*var valid = bbm.validator.validateDocumentModel(result);
-
-        if (!valid) {
-            console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-        }
-
-        expect(valid).to.be.true;
-        */
-
     });
 
-    /*    it('Extra line breaks before sections', function (done) {
-            var txtfile = fs.readFileSync(__dirname + '/fixtures/sample-extra.txt', 'utf-8');
-
-            expect(txtfile).to.exist;
-
-            //convert string into JSON
-            var result = bbcms.parseText(txtfile);
-            expect(result).to.exist;
-
-            var valid = bbm.validator.validateDocumentModel(result);
-
-            if (!valid) {
-                console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-            }
-
-            expect(valid).to.be.true;
-
-            done();
-        });
-    */
 });
