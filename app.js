dd58ec3857fb8c7f840ada2378fbe972e3bd07f0
Alex S <alexey.shchelokov@gmail.com>
2015-09-02 11:37:34 -0400

    test: parameter list

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var fs = require('fs');
 var split = require('split');
 
 var bbcms = require("./index");
-var bbm = require('blue-button-model');
+//var bbm = require('blue-button-model');
 
 var istream = fs.createReadStream(__dirname + '/test/fixtures/sample.txt', 'utf-8');
 
 istream.pipe(split())
     .pipe(new bbcms.CmsFile2Object())
-    .pipe(new bbcms.IntObjToFhirStream("http://localhost:8080/fhir/base"))
+    .pipe(new bbcms.IntObjToFhirStream("test","http://localhost:8080/fhir/base"))
     .on('data', function (data) {
         console.log(JSON.stringify(data, null, '  '));
     })
