e31fad1638e08b973e9838518391d28eff257a14
Alex S <alexey.shchelokov@gmail.com>
2015-09-30 16:59:48 -0400

    style: formatting

--- app.js
+++ app.js
@@ -41,51 +41,57 @@ istream.pipe(split())
                 });
                 res.on('end', function () {
                     console.log('No more data in response.');
-                    var zipped = _.zip( data.entry, JSON.parse(response).entry);
+                    var zipped = _.zip(data.entry, JSON.parse(response).entry);
                     console.log(zipped);
-                    
-                    zipped.forEach(function(element) {
+
+                    zipped.forEach(function (element) {
                         var req = http.request({
-                hostname: 'localhost',
-                port: 8080,
-                path: '/fhir/baseDstu2/' + element[1].response.location,
-                method: 'GET',
-                        }, function(res) {
+                            hostname: 'localhost',
+                            port: 8080,
+                            path: '/fhir/baseDstu2/' + element[1].response.location,
+                            method: 'GET',
+                        }, function (res) {
                             res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    //console.log('BODY2: ' + chunk);
-                    var resource = JSON.parse(chunk);
-                    var comparator = function(l,r, propn) {
-                        if(_.isNumber(l)) return;
-                        if(_.isString(l)) return;
-                        if(_.isBoolean(l)) return;
-                        if(_.isArray(l) && _.isArray(r)) {
-                            if( l.length !== r.length) {
-                                console.log('!-------- Array length differ %s', propn);
-                            }
-                            for(var i = 0; i< l.lenght && i<r.length; i++) {
-                                comparator( l[i], r[i], propn);
-                            }
-                        }
-                        
-                        for(var prop in l) {
-                            if( l.hasOwnProperty(prop)) {
-                                if(!r.hasOwnProperty(prop)) {
-                                    console.log('!-------- Missed prop ' + prop);
-                                } else {
-                                    comparator(l[prop],r[prop], prop);
-                                }
-                            }
-                        }
-                    };
-                    console.log( 'compare:\n %j \n %j', element[0].resource, resource);
-                    comparator( element[0].resource, resource);
-                });
-                        } );
-                        req.end();       
+                            res.on('data', function (chunk) {
+                                //console.log('BODY2: ' + chunk);
+                                var resource = JSON.parse(chunk);
+                                var comparator = function (l, r, propn) {
+                                    if (_.isNumber(l)) {
+                                        return;
+                                    }
+                                    if (_.isString(l)) {
+                                        return;
+                                    }
+                                    if (_.isBoolean(l)) {
+                                        return;
+                                    }
+                                    if (_.isArray(l) && _.isArray(r)) {
+                                        if (l.length !== r.length) {
+                                            console.log('!-------- Array length differ %s', propn);
+                                        }
+                                        for (var i = 0; i < l.lenght && i < r.length; i++) {
+                                            comparator(l[i], r[i], propn);
+                                        }
+                                    }
+
+                                    for (var prop in l) {
+                                        if (l.hasOwnProperty(prop)) {
+                                            if (!r.hasOwnProperty(prop)) {
+                                                console.log('!-------- Missed prop ' + prop);
+                                            } else {
+                                                comparator(l[prop], r[prop], prop);
+                                            }
+                                        }
+                                    }
+                                };
+                                console.log('compare:\n %j \n %j', element[0].resource, resource);
+                                comparator(element[0].resource, resource);
+                            });
+                        });
+                        req.end();
                     });
                     //process.exit();
-                })
+                });
             });
         req.write(bundle);
         req.end();
